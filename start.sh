15041d0c68a57b41439aba98886397ffca419095
Gabriel <ga.gambel@gmail.com>
2024-10-23 01:03:35 +0200

    changed start.sh

--- start.sh
+++ start.sh
@@ -5,20 +5,14 @@ if [ -f ./.venv/bin/activate ]; then
     source .venv/bin/activate
     python main.py
 else
-    PYV=$(python --version 2>&1)
-    if [[ $PYV = *"3.12.3" ]]; then
-        echo "Setting up virtuell env .."
-        python3.12 -m venv .venv
-        source .venv/bin/activate
-        echo "Installing requirements .."
-        python -m pip install -r requirements.txt
-        echo "Starting application .."
-        python main.py
-    else
-        echo "$PYV is the wrong version of python, please us 3.12.3 for this application"
-        echo "use sudo zypper install python312 to get the package and rerun this script"
-        echo "instead of zypper any other packet manager is usebale obviously"
-        echo "If you have issues please check the README.txt or contact someone for help"
-    fi
-
+    echo "installing missing module :"
+    sudo zypper install libgthread-2_0-0 libgthread-2_0-0-32bit
+    echo "Setting up virtuel env .."
+    python3.12 -m venv .venv
+    source .venv/bin/activate
+    echo "Installing requirements .."
+    python -m pip install -r requirements.txt
+    echo "Starting application .."
+    python main.py
 fi
+deactivate
